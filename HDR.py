f11f6336b23fcaa752a86de0a1db752c20d5b6e9
Sondos Lahiwal <Sondoslhiwal@gmail.com>
2025-11-01 15:25:30 +0200

    last one

--- HDR.py
+++ HDR.py
@@ -6,3 +6,14 @@ from sklearn.model_selection import train_test_split
 (x_train,y_train),(x_test,y_test)=minst.load_data()
 x_train = x_train / 255.0
 x_test = x_test / 255.0
+Brainstorm_Model=Sequential([
+   Flatten(input_shape=(28,28)),
+   Dense(128,activation='relu'),
+   Dropout(0.2),
+   Dense(10,activation='softmax')])
+
+
+Brainstorm_Model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+X =Brainstorm_Model.fit(x_train,y_train,validation_data=(x_val,y_val),epochs=20)
+
+print(Brainstorm_Model.evaluate(x_test,y_test))
