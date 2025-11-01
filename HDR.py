6ad9fbbf5796381c00da95afc36d3bc5765d1a76
Sondos Lahiwal <Sondoslhiwal@gmail.com>
2025-11-01 15:22:51 +0200

    second commit

--- HDR.py
+++ HDR.py
@@ -2,3 +2,7 @@ from tensorflow.keras.models import sequential
 from tensorflow.keras.dataset import minst
 from tensorflow.keras.layers import Dense,Dropout,Flatten 
 from sklearn.model_selection import train_test_split
+
+(x_train,y_train),(x_test,y_test)=minst.load_data()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
