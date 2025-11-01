9c19078c9571856d7f11b36ce9a164ac22c70bb9
Sondos Lahiwal <Sondoslhiwal@gmail.com>
2025-11-01 13:57:15 +0200

    init: add hand digit recognition project

---
+++ HDR.py
@@ -0,0 +1,4 @@
+from tensorflow.keras.models import sequential
+from tensorflow.keras.dataset import minst
+from tensorflow.keras.layers import Dense,Dropout,Flatten 
+from sklearn.model_selection import train_test_split
